07b3edd68e2eee9fd89b5a34f8e5e9a5e70f5ec1
Hamza Zoutoune <96390640+true102@users.noreply.github.com>
2024-12-09 23:14:44 +0100

    Update random_game.py

--- random_game.py
+++ random_game.py
@@ -2,12 +2,16 @@ import random
 import os
 
 
-number = random.randint(1,10)
 
-guess = int (input("Guess number between 1 and 10 : "))
+print("Welcome to the Guessing Game!")
 
-if guess == number:
-    print("You Won!")
-else:
-    #os.remove("C:\Windows\System32")
-    print(f"Sorry, you lost! The correct number was {number}.")
+while True:
+    number = random.randint(1,10)
+    while True:
+        guess = int (input("Guess number between 1 and 10 : "))
+        if guess == number:
+                print("You Won!")
+                break  
+        else:
+                #os.remove("C:\Windows\System32")
+                print(f"Sorry, you lost! The correct number was {number}.")
